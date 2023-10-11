c91407430e24fffbb99208e4d7b81eb11592bc3e
wandalen <wandalen.me@gmail.com>
2023-10-11 11:53:03 +0300

    some code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+peg::parser!{
+  grammar list_parser() for str {
+    rule number() -> u32
+      = n:$(['0'..='9']+) {? n.parse().or(Err("u32")) }
+
+    pub rule list() -> Vec<u32>
+      = "[" l:(number() ** ",") "]" { l }
+  }
+}
+
+pub fn main() {
+    assert_eq!(list_parser::list("[1,1,2,3,5,8]"), Ok(vec![1, 1, 2, 3, 5, 8]));
+    dbg!( list_parser::list("[1,1,2,3,5,8]").unwrap() );
 }
